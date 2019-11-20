b51648194cf91fa8d85ef59c17c2a4a79e5a6a9c
doris petrisoru <doris.petrisoru@gmail.com>
2019-11-20 12:01:52 +0200

    update week 4 session 2

---
+++ Exercises/Models/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Exercises.Models
+{
+    public class CourseModel
+    {
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public List<StudentModel> Students { get; set; }
+    }
+}
---
+++ Exercises/Services/CourseService.cs
@@ -0,0 +1,20 @@
+﻿
+using Exercises.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Exercises.Services
+{
+    public class CourseService
+    {
+        public void AddStudentsToCourse(CourseModel course, List<StudentModel> students)
+        {
+            if (course == null || course.Students == null)
+            {
+                throw new Exception("Course or Students cannot be null");
+            }
+
+            course.Students.AddRange(students);
+        }
+    }
+}
