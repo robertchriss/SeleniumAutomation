749e31023695016c7fae03bd54eb31c93cf5616f
doris petrisoru <doris.petrisoru@gmail.com>
2020-01-06 13:05:04 +0200

    added test data for login + encryption class

--- UnitTesting/seleniumTests/LoginTest.cs
+++ UnitTesting/seleniumTests/LoginTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Exercises.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -17,6 +18,13 @@ namespace UnitTesting.seleniumTests
             driver.Navigate().GoToUrl("https://www.ibm.com/ro-en");
             driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[contains(@id, 'pop-frame')]")));
             driver.FindElement(By.ClassName("call")).Click();
+
+            #region test data helper
+            //you cand use as IBMid (username): "doris.petrisoru@yahoo.com"
+            //encrypted password: "YhRBMTukHZI8xTL4be7sD3yD98ulI+ZbwvlMowH7UEGFTpCt9ibMJ2DbUJWQlgtRRIlwzEjGUGwl4tg5KROdsJFecfuF/k/d8I7G/JXj3tAJcCsNIp9Wp1rIRC0mdbjf"
+            //decyption key: "tESTaUTOMATION"
+            //to decrypt pwd at run time use: EncryptDecryptHelper.Decrypt(encrypted_pwd, key);
+            #endregion
         }
     }
 }
