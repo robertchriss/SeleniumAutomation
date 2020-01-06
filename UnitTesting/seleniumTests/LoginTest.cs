2246a18e50c688665bc4456621d6803ef3e49f1e
doris petrisoru <doris.petrisoru@gmail.com>
2020-01-06 11:53:13 +0200

    week 9, session 1

---
+++ UnitTesting/seleniumTests/LoginTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace UnitTesting.seleniumTests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TC01_NavigateToIbmSite()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://www.ibm.com/ro-en");
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[contains(@id, 'pop-frame')]")));
+            driver.FindElement(By.ClassName("call")).Click();
+        }
+    }
+}
