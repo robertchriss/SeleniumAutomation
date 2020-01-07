097cf986901c445b815430d09583f8f7a6c9acb0
doris petrisoru <doris.petrisoru@gmail.com>
2020-01-07 20:33:03 +0200

    added Homework week 9, session 1

--- Exercises/Helpers/Constants.cs
+++ Exercises/Helpers/Constants.cs
@@ -8,6 +8,8 @@ namespace Exercises.Helpers
 {
     public class Constants
     {
-        public static readonly String STRINGPHRASE = "tESTaUTOMATION";
+        public static readonly string STRINGPHRASE = "tESTaUTOMATION";
+        public static readonly string USERNAME = "doris.petrisoru@yahoo.com";
+        public static readonly string ENCODED_PASS = "YhRBMTukHZI8xTL4be7sD3yD98ulI+ZbwvlMowH7UEGFTpCt9ibMJ2DbUJWQlgtRRIlwzEjGUGwl4tg5KROdsJFecfuF/k/d8I7G/JXj3tAJcCsNIp9Wp1rIRC0mdbjf";
     }
 }
--- UnitTesting/seleniumTests/LoginTest.cs
+++ UnitTesting/seleniumTests/LoginTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 
 namespace UnitTesting.seleniumTests
@@ -9,6 +10,7 @@ namespace UnitTesting.seleniumTests
     [TestClass]
     public class LoginTest
     {
+
         [TestMethod]
         public void TC01_NavigateToIbmSite()
         {
@@ -17,8 +19,19 @@ namespace UnitTesting.seleniumTests
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Navigate().GoToUrl("https://www.ibm.com/ro-en");
             driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[contains(@id, 'pop-frame')]")));
+        
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+            wait.Until(d => d.FindElement(By.ClassName("call")).Displayed);    
             driver.FindElement(By.ClassName("call")).Click();
 
+            driver.SwitchTo().DefaultContent();
+            driver.FindElement(By.ClassName("ibm-profile-link")).Click();
+            driver.FindElement(By.XPath("//li[@data-linktype='signin']/a")).Click();
+            driver.FindElement(By.Id("username")).SendKeys(Constants.USERNAME);
+            driver.FindElement(By.Id("continue-button")).Click();
+            driver.FindElement(By.Id("password")).SendKeys(EncryptDecryptHelper.Decrypt(Constants.ENCODED_PASS));
+            driver.FindElement(By.Id("signinbutton")).Click();
+            
             #region test data helper
             //you cand use as IBMid (username): "doris.petrisoru@yahoo.com"
             //encrypted password: "YhRBMTukHZI8xTL4be7sD3yD98ulI+ZbwvlMowH7UEGFTpCt9ibMJ2DbUJWQlgtRRIlwzEjGUGwl4tg5KROdsJFecfuF/k/d8I7G/JXj3tAJcCsNIp9Wp1rIRC0mdbjf"
