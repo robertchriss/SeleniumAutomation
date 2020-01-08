444e42eeb09afb494179d170ae258da75f3c52d5
Chris Ryan <chriss.ercau@gmail.com>
2020-01-08 09:47:57 +0200

    Added students model and student service

---
+++ Exercises/Models/StudentsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Models
+{
+    public class StudentsModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Exercises/Services/StudentServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercises.Models;
+
+namespace Exercises.Services
+{
+    public class StudentServices
+    {
+        public List <StudentsModel> CreatesStudentsFromFullNameList(List<string> studentlist)
+        {
+            List<StudentsModel> returnedList = new List<StudentsModel>();
+
+           // foreach(string student in StudentsList)
+            //{
+              //  string[] fullNameArray = student.Split(' ');
+          //  }
+            return returnedList;
+        }
+    }
+}
